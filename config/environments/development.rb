400df332854ce3831cbd7e31d6b13a7d2937b220
Jeff Deville <jeffdeville@gmail.com>
2013-12-25 17:23:12 -0500

    Allow web connections in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ Mixmeup::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  WebMock.allow_net_connect!
+
 end
